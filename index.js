6b092996fd64ec0356f8683e2263aef5c1613482
Fajar Sidik Priatna <fajar@suitmedia.com>
2020-10-25 16:18:20 +0700

    fix tester port 80

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const fetch = require('node-fetch')
 var FormData = require('form-data');
-const { delay } = require('lodash');
+const connectionTester = require('connection-tester');
 const log = require('simple-node-logger').createSimpleLogger('project.log');
-const { spawn } = require("child_process");
-// let ip = 131174112652
+// let ip = 131174112653
 
-let base = 131174112400
+let base = 131174112652
 let index = 0;
 var url = 'https://ip-dynamic.com/home/getUsageOnline'
 
@@ -15,32 +14,37 @@ function getApi(url, base, index) {
     if (index == 300)
         return
 
-    const params = new FormData();
     const indihomeIp = base + index
+    const params = new FormData();
     params.append('no_indihome', `${indihomeIp}`);
     params.append('signature', '6362aaf1b46ef9ccfd4d8dd292deb8a1');
-
+    //staging 541045b6d5a72528406c021abe4bf06
     fetch(url, {
         method: 'post',
         body: params
     }).then((response) => {
         response.text().then(function (data) {
             if (data != 0) {
-                const check = spawn("timeout", ["1", "nc", "-z", "-v", data, "80"])
-                check.stdout.on('data', (data) => {
-                    var kuy = `${(base + index)}: http://${data}`;
-                    log.info(kuy)
-                    // console.log(`stdout: ${data}`);
-                });
-
-                check.stderr.on('data', (data) => {
-                    // console.error(`stderr: ${data}`);
-                });
+                console.log(indihomeIp)
+                connectionTester.test(
+                    data,
+                    80,
+                    1500,
+                    (err, output) => {
+                        if (output.success == true) {
+                            var kuy = `${indihomeIp}: http://${data}`;
+                            log.info(kuy)
+                        } else {
+                            console.log('port 80 closed')
+                        }
+                    }
+                );
             }
         })
         index++
         getApi(url, base, index)
     }).catch((err) => {
+        console.log('failed')
         index++
         getApi(url, base, index)
         return null
